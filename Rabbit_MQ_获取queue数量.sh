4a61d727eaa35e3f545926fcc74707885dc0e65a
Smile Black Offee <YanSiNan@SmileMacBook-Pro.local>
2016-08-23 18:44:29 +0800

    过滤同类字段多次出现问题

--- Rabbit_MQ_获取queue数量.sh
+++ Rabbit_MQ_获取queue数量.sh
@@ -5,11 +5,24 @@ for virtual_host in `rabbitmqctl list_vhosts | grep -v 'Listing queues ...' | gr
 do
     for queue_name in `rabbitmqctl list_queues -p $virtual_host name |grep -v 'Listing queues ...'`
     do
-        for  queue_name_num in `rabbitmqctl list_queues -p $virtual_host | grep $queue_name |grep -v 'Listing queues ...' | awk '{print $2}'`
-        do
-            echo $virtual_host.$queue_name=$queue_name_num
-            queue_sum=$[$queue_sum+$queue_name_num]
-        done
+#	for  queue_name_num in `rabbitmqctl list_queues -p $virtual_host | grep $queue_name |grep -v 'Listing queues ...' | awk '{print $2}'`
+#	do
+#		echo $virtual_host.$queue_name=$queue_name_num
+#		queue_sum=$[$queue_sum+$queue_name_num]
+#	done
+	if [[ $queue_name = "normandy.q" ]];then
+		for  queue_name_num in `rabbitmqctl list_queues -p $virtual_host | grep $queue_name | grep -v "message_normandy.q" |awk '{print $2}'`
+		do
+			echo $virtual_host.$queue_name=$queue_name_num
+			queue_sum=$[$queue_sum+$queue_name_num]
+		done
+	else
+		for  queue_name_num in `rabbitmqctl list_queues -p $virtual_host | grep $queue_name  |awk '{print $2}'`
+		do
+            		echo $virtual_host.$queue_name=$queue_name_num
+            		queue_sum=$[$queue_sum+$queue_name_num]
+       		done
+	fi
     done
 done
 echo -e "RabbitMQ_Ready=$queue_sum"
